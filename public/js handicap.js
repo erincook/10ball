4593a2a6d70deffd2e70c77b796606fcbedf6b3a
Chris Woodrum <chriswoodrum2011@gmail.com>
2017-10-11 15:32:49 -0400

    Add files via upload

---
+++ public/js handicap.js
@@ -0,0 +1,13 @@
+var scores = [s,s,s,s,s,s,s,s,s,s,s,s,]
+var last10
+if (scores lengths = 10){
+   last10 = scores.slice(scores length 10,10);
+} else}
+   last10 = scores
+}
+var sum = 0
+last10.for each(function(x){
+sum + = x
+})
+var avg=sum/last10.lengthvar handicap=(50-sum)/2
+console.log(handicap)   
