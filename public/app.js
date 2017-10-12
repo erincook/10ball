5639f824dc2b3ca3e7811af34256219bc114097e
Damnuel Coleman West <daltonwest2@gmail.com>
2017-10-11 19:13:24 -0700

    Added App.js
    Trying to link firebase. Created the banner in hi definition.

---
+++ public/app.js
@@ -0,0 +1,19 @@
+﻿//Connect to firebase
+var config = {
+    apiKey: "AIzaSyCD1dJiTpIv0MygHTH5J97FHVl6QnMZNFK",
+    authDomain: "ball-6af20.firbaseapp.com",
+    databaseURL: "https://ball-6af20.firbaseio.com",
+    projectID: "ball-6af20",
+    storageBucket: "ball-6af20.appspot.com",
+    messagingSenderId: "48314393073",
+};
+firebase.initializeApp(config);
+
+//register a user
+
+
+// login a user
+
+
+//coonect to data to store other info
+
