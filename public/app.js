6227485dfeb00e05ea43bae18a98ea1f62d318fa
Damnuel Coleman West <daltonwest2@gmail.com>
2017-10-11 21:32:47 -0700

    Signup Code
    Modified Signup Code

--- public/app.js
+++ public/app.js
@@ -1,16 +1,43 @@
 ﻿//Connect to firebase
 var config = {
-    apiKey: "AIzaSyCD1dJiTpIv0MygHTH5J97FHVl6QnMZNFK",
-    authDomain: "ball-6af20.firbaseapp.com",
-    databaseURL: "https://ball-6af20.firbaseio.com",
-    projectID: "ball-6af20",
+    apiKey: "AIzaSyCD1dJiTpIv0MygHTH5J97FHVl6QnMZNFk",
+    authDomain: "ball-6af20.firebaseapp.com",
+    databaseURL: "https://ball-6af20.firebaseio.com",
+    projectId: "ball-6af20",
     storageBucket: "ball-6af20.appspot.com",
-    messagingSenderId: "48314393073",
+    messagingSenderId: "48314393073"
 };
 firebase.initializeApp(config);
 
 //register a user
-
+function userSignUp() {
+    var email = document.getElementById('userEmail').value;
+    var password = document.getElementById('userPassword').value;
+    if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+    }
+    if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+    }
+    // Sign in with email and pass.
+    // [START createwithemail]
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+    });
+    // [END createwithemail]
+}
 
 // login a user
 
